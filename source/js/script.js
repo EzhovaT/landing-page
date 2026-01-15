f8b46544083ecb19d214d71382ebaab07197c67d
Tatyana <tatyezhova@yandex.ru>
2026-01-15 19:15:40 +0300

    fix gulpfile

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,12 @@ const mainNav = document.querySelector(".main-nav"),
   optionsSlider = document.querySelector(".options__inner-wrapper"),
   optionsSliderWrapper = document.querySelector(".options__slider-tape"),
   pricesSlider = document.querySelector(".prices__inner-wrapper"),
-  pricesSliderWrappe = document.querySelector(".prices__slider-tape");
+  pricesSliderWrappe = document.querySelector(".prices__slider-tape"),
+  popupOpeningBtn = document.querySelectorAll(".btn-start"),
+  page = document.querySelector(".page"),
+  form =  document.getElementById("popup__form"),
+  submitBtn = form.submit,
+  productPlans = document.querySelectorAll("input[name='productPlan'");
 
 
 //open mobile menu
@@ -16,7 +21,71 @@ mainNav.addEventListener("click", (e) => {
   }
 });
 
+//popup
+let unlock = true;
 
+if (popupOpeningBtn.length > 0) {
+  for (let i = 0; i < popupOpeningBtn.length; i++) {
+    const popupLink = popupOpeningBtn[i];
+    popupLink.addEventListener('click', (e) => {
+      const currentPopup = document.getElementById('popup-call');
+      popupOpen(currentPopup);
+    })
+  }
+}
+
+function popupOpen(currentPopup) {
+  if(currentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add('open');
+    currentPopup.addEventListener('click', (e) => {
+      if(!e.target.closest('.popup__content-wrapper')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    })
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+      bodyUnLock();
+    }
+  }
+}
+
+function bodyLock() {
+  page.classList.add('lock');
+
+  unlock = false;
+  setTimeout(function() {
+    unlock = true;
+  }, 800);
+}
+
+function bodyUnLock() {
+  setTimeout(function() {
+    page.classList.remove('lock');
+  }, 800);
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, 800);
+}
+
+// submitBtn.addEventListener('click', validateForm)
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const successPopup = document.getElementById('popup-success');
+  popupOpen(successPopup);
+})
 //slider
 
 let offsetLeft = 0;
@@ -62,19 +131,6 @@ pricesSlider.addEventListener("click", (e) => {
   }
 });
 
-//scroll page
-
-const taglineLink = document.querySelector(".tagline__link-price");
-
-taglineLink.addEventListener("click", function (e) {
-  e.preventDefault();
-  const blockID = taglineLink.getAttribute("href");
-  document.querySelector(blockID).scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-});
-
 //Learn more
 
 const advantages = document.querySelectorAll(".advantages__link");
@@ -89,3 +145,34 @@ advantages.forEach((element) => {
     }
   });
 });
+
+
+// Product
+
+productPlans.forEach(element => {
+  const cards = document.querySelectorAll("p.price-card__price");
+  const yearlyPlan = [
+    '<b>Free</b>',
+    '<b>$29</b>/month',
+    '<b>$49</b>/month'
+  ];
+
+  const monthlyPlan = [
+    '<b>Free</b>',
+    '<b>$10</b>/month',
+    '<b>$20</b>/month'
+  ];
+
+  element.addEventListener('change', (e) => {
+    let plan;
+    if(e.target.id === 'Yearly') {
+      plan = yearlyPlan;
+    } else {
+      plan = monthlyPlan;
+    }
+    for (let index = 0; index < cards.length; index++) {
+      cards[index].innerHTML = plan[index];
+    }
+  })
+});
+
